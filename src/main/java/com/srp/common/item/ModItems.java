7701ac3c47aeeb9ec4b2fd347a8553fe781c595e
Sunconure11 <eyespot7b@aol.com>
2018-02-16 11:49:53 -0500

    Cas9

---
+++ src/main/java/com/srp/common/item/ModItems.java
@@ -0,0 +1,14 @@
+package com.srp.common.item;
+
+import com.srp.common.lib.LibMod;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+/**
+ * Created by Joseph on 2/16/2018.
+ */
+@SuppressWarnings("ConstantConditions")
+@GameRegistry.ObjectHolder(LibMod.MOD_ID)
+public final class ModItems {
+	private static final Item PLACE_HOLDER = new Item();
+}
