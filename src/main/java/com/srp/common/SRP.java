4c990bd1e9f43e6a51655287f8214e7f9e8579c0
Sunconure11 <eyespot7b@aol.com>
2018-02-16 09:50:04 -0500

    Crispr

--- src/main/java/com/srp/common/SRP.java
+++ src/main/java/com/srp/common/SRP.java
@@ -1,7 +1,14 @@
 package com.srp.common;
 
+import com.srp.common.lib.LibMod;
+import net.minecraftforge.fml.common.Mod;
+
+import static com.srp.common.lib.LibMod.MOD_NAME;
+
 /**
  * Created by Joseph on 2/15/2018.
  */
+@SuppressWarnings("WeakerAccess")
+@Mod(modid = LibMod.MOD_ID, name = MOD_NAME, version = LibMod.MOD_VER, dependencies = LibMod.DEPENDENCIES, acceptedMinecraftVersions = "[1.12,1.13]")
 public class SRP {
 }
---
+++ src/main/java/com/srp/common/lib/LibMod.java
@@ -0,0 +1,30 @@
+package com.srp.common.lib;
+
+/**
+ * This class was created by <Arekkuusu> on 26/02/2017.
+ * It's distributed as part of Bewitchment under
+ * the MIT license.
+ */
+public final class LibMod {
+
+	//ID for MOD
+	public static final String MOD_ID = "sunsroleplayitems";
+
+	//Name of MOD
+	public static final String MOD_NAME = "Suns_Roleplay_Items";
+
+	//Version of MOD
+	public static final String MOD_VER = "@VERSION@";
+
+	//Dependency
+	public static final String DEPENDENCIES = "required-after:forge@[14.23.2.2611,]";
+
+	//Client proxy location
+	public static final String PROXY_CLIENT = "com.srp.client.core.ClientProxy";
+
+	//Server proxy location
+	public static final String PROXY_COMMON = "com.srp.common.core.proxy.ServerProxy";
+
+	private LibMod() {
+	}
+}
