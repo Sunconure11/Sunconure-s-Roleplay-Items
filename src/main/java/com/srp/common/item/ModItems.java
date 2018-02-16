42ffe661dc4034df0839855c4fda53b7a8523c11
Sunconure11 <eyespot7b@aol.com>
2018-02-16 11:50:47 -0500

    Stuff

--- src/main/java/com/srp/common/item/ModItems.java
+++ src/main/java/com/srp/common/item/ModItems.java
@@ -3,6 +3,7 @@ package com.srp.common.item;
 import com.srp.common.lib.LibMod;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.registries.IForgeRegistry;
 
 /**
  * Created by Joseph on 2/16/2018.
@@ -11,4 +12,10 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 @GameRegistry.ObjectHolder(LibMod.MOD_ID)
 public final class ModItems {
 	private static final Item PLACE_HOLDER = new Item();
+
+	private ModItems() {
+	}
+	public static void register(final IForgeRegistry<Item> registry) {
+
+	}
 }
