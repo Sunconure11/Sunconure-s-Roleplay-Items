f7c0a97f47f1e295dd66dc4875fea8d16b8b924a
Sunconure11 <eyespot7b@aol.com>
2018-02-16 14:26:09 -0500

    reformat

--- src/main/java/com/srp/common/block/BlockMod.java
+++ src/main/java/com/srp/common/block/BlockMod.java
@@ -8,7 +8,6 @@ import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
