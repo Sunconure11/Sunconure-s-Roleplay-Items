4ccd8e5808c6370c3b5612e65d61517def453ff9
Sunconure11 <eyespot7b@aol.com>
2018-02-16 11:56:29 -0500

    more WIP stuff

--- src/main/java/com/srp/common/item/ModItems.java
+++ src/main/java/com/srp/common/item/ModItems.java
@@ -1,7 +1,10 @@
 package com.srp.common.item;
 
 import com.srp.common.lib.LibMod;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.fml.common.LoaderException;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
 
@@ -16,6 +19,27 @@ public final class ModItems {
 	private ModItems() {
 	}
 	public static void register(final IForgeRegistry<Item> registry) {
+		registry.registerAll(
+		);
+	}
+	private static Item itemBlock(Block block) {
+		if (block == null) {
+			throw new LoaderException("[" + LibMod.MOD_NAME + "] Trying to register an ItemBlock for a null block");
+		}
+		if (block.getRegistryName() == null) {
+			throw new LoaderException("[" + LibMod.MOD_NAME + "] Trying to register an ItemBlock for a block with null name - " + block.getUnlocalizedName());
+		}
+		if (block.getRegistryName().toString() == null) {
+			throw new LoaderException("[" + LibMod.MOD_NAME + "] There's something wrong with the registry implementation of " + block.getUnlocalizedName());
+		}
+		return new ItemBlock(block).setRegistryName(block.getRegistryName());
+	}
+
+	public static void init() {
+		initOreDictionary();
+	}
+	
+	private static void initOreDictionary() {
 
 	}
 }
